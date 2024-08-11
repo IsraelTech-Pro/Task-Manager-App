31cad470e623a0e060128c4f3adf09989573dbe1
IsraelTech-Pro <israelopoku360@gmail.com>
2024-08-11 07:45:46 +0100

    Add build script for Render

--- render-build.sh
+++ render-build.sh
@@ -4,6 +4,7 @@ set -o errexit
 
 wget https://download.visualstudio.microsoft.com/download/pr/d3e46476-4494-41b7-a628-c517794c5a6a/6066215f6c0a18b070e8e6e8b715de0b/dotnet-sdk-6.0.402-linux-x64.tar.gz 
 
+
 mkdir -p $XDG_CACHE_HOME/dotnet && tar zxf dotnet-sdk-6.0.402-linux-x64.tar.gz -C $XDG_CACHE_HOME/dotnet
 
 
